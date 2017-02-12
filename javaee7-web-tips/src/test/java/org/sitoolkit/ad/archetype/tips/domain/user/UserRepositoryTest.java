c9b303b0c7b17e037f78f4bce2c9c909937c82ab
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2017-02-12 15:39:06 +0900

    テストのトランザクション制御にDeltaSpikeを使用

--- javaee7-web-tips/src/test/java/org/sitoolkit/ad/archetype/tips/domain/user/UserRepositoryTest.java
+++ javaee7-web-tips/src/test/java/org/sitoolkit/ad/archetype/tips/domain/user/UserRepositoryTest.java
@@ -5,36 +5,29 @@ import static org.hamcrest.Matchers.*;
 
 import java.util.List;
 
+import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.deltaspike.jpa.api.transaction.Transactional;
 import org.apache.deltaspike.testcontrol.api.junit.CdiTestRunner;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.sitoolkit.ad.archetype.tips.infrastructure.data.jpa.RollbackRule;
-import org.sitoolkit.ad.archetype.tips.test.TestCaseLog;
 
+@Dependent
 @RunWith(CdiTestRunner.class)
 public class UserRepositoryTest {
-
-    @Rule
-    public TestCaseLog log = new TestCaseLog(getClass());
-
-    @Inject
-    @Rule
-    public RollbackRule rollbackRule;
 
     @Inject
     UserRepository repo;
 
     @Test
+    @Transactional(readOnly = true)
     public void testSelectByLastName() {
         UserEntity user = UserFactory.create();
         user.setLastName(RandomStringUtils.randomAlphanumeric(32));
 
         repo.create(user);
-        repo.em().flush();
 
         List<UserEntity> result = repo.selectByLastName(user.getLastName());
 
